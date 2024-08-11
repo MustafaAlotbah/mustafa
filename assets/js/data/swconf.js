2ac5203299dff0b92a9fb14585781ded5afa8a23
Mustafa Alotbah <mustafa.alotbah@gmail.com>
2024-08-11 20:54:47 +0200

    Old Posts

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723377253',resources: [
+    cacheName: 'chirpy-1723402313',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
